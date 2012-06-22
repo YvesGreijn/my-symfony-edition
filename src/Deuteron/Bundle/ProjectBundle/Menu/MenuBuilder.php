7f4f0603c065fa89fc6f42d0300fce692960d4b5
Maxime AILLOUD <maxime.ailloud@gmail.com>
2012-06-22 14:52:00 +0200

    Ajout du menu global de l'application

---
+++ src/Deuteron/Bundle/ProjectBundle/Menu/MenuBuilder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Deuteron\Bundle\ProjectBundle\Menu;
+
+use Knp\Menu\FactoryInterface,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\Routing\Router,
+    Symfony\Component\Security\Core\SecurityContext
+;
+
+class MenuBuilder
+{
+  /**
+   * @var \Knp\Menu\FactoryInterface
+   */
+  private $factory;
+
+  /**
+   * @var \Symfony\Component\Security\Core\SecurityContext
+   */
+  private $securityContext;
+
+  /**
+   * @param \Knp\Menu\FactoryInterface $factory
+   * @param \Symfony\Component\Security\Core\SecurityContext $securityContext
+   */
+  public function __construct(FactoryInterface $factory, SecurityContext $securityContext)
+  {
+    $this->factory = $factory;
+
+    $this->securityContext = $securityContext;
+  }
+
+  /**
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   * @return mixed
+   */
+  public function createMainMenu(Request $request)
+  {
+    $menu = $this->factory->createItem('root');
+
+    $menu->addChild('A propos',  array('route' => 'about'));
+
+    if(true === $this->securityContext->isGranted('ROLE_SUPER_ADMIN'))
+    {
+      $menu
+        ->addChild('Administration',  array('route' => 'adminDashBoard'))
+      ;
+    }
+
+    return $menu;
+  }
+}
